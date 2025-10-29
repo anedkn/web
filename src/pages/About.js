463a03ba9a7c6f6711095b435e67aa9320cc652c
Едакина Ангелина Викторовна <23629@dune.edu>
2025-10-29 11:03:12 +0200

    Добавлен роутер, сохранён локальный проект web-main в ветку save-local

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div style={{ padding: 16 }}>
+      <h1>Изменения проекта:</h1>
+      <p>Добавлена маршрутизация (React Router).</p>
+    </div>
+  );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import Form from "../Form";
+import Table from "../Table";
+import EmployeeService from "../api/service";
+
+export default function Home() {
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    setPeople(EmployeeService.all());
+  }, []);
+
+  const addPerson = (person) => {
+    setPeople(EmployeeService.add(person));
+  };
+
+  const deletePerson = (id) => {
+    setPeople(EmployeeService.delete(id));
+  };
+
+  return (
+    <div>
+      <h1>Список людей</h1>
+      <Form onAdd={addPerson} />
+      <Table people={people} onDelete={deletePerson} />
+    </div>
+  );
+}
